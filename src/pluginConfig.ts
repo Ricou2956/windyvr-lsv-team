bd32596172cea6be4c3277ce27120ca1d346d0db
Ricou2956 <Ricou63lety@gmail.com>
2026-08-25 23:28:00 +0200

    Bump version from 1.0.1 to 1.0.2

--- src/pluginConfig.ts
+++ src/pluginConfig.ts
@@ -1,10 +1,11 @@
 const config = {
   name: 'windy-plugin-windyvr-lsv-team',
-  version: '1.0.1',
+  version: '1.0.2',
   icon: '⛵',
   title: 'WindyVR LSV team',
   description: 'Compare jusqu’à quatre routages VR avec ECMWF, GFS et ICON au temps Windy sélectionné.',
   author: 'WindyVR LSV team',
+  repository: 'https://github.com/Ricou2956/windyvr-lsv-team',
   desktopUI: 'rhpane',
   desktopWidth: 520,
   mobileUI: 'fullscreen',
